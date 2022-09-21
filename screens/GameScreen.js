83aaa1b6c244395c2f625d1f19f9fcab8921135a
brianmuturi2 <brianmuturi2@gmail.com>
2022-09-21 18:27:18 +0300

    feat(guess game): updated screens width landscape mode

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import {Text, View, StyleSheet, Alert, FlatList} from 'react-native';
+import {Text, View, StyleSheet, Alert, FlatList, useWindowDimensions } from 'react-native';
 import Title from '../components/UI/Title';
 import generateRandomBetween from '../random';
 import {useState, useEffect} from 'react';
@@ -17,6 +17,8 @@ function GameScreen({userNumber, onGameOver}) {
     const initialGuess = generateRandomBetween(1, 100, userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [guessRounds, setGuessRounds] = useState([initialGuess]);
+
+    const {width, height} = useWindowDimensions();
 
     useEffect(() => {
         if (currentGuess === userNumber) {
@@ -49,9 +51,8 @@ function GameScreen({userNumber, onGameOver}) {
 
     const guessRoundsListLength = guessRounds.length;
 
-    return (
-        <View style={styles.screen}>
-            <Title>Opponent's Guess</Title>
+    let content = (
+        <>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card>
                 <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
@@ -68,6 +69,33 @@ function GameScreen({userNumber, onGameOver}) {
                     </View>
                 </View>
             </Card>
+        </>
+    );
+
+    if (width > 500) {
+        content = (
+            <>
+                <View style={styles.buttonContainerWide}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'upper')}>
+                            <Ionicons name="md-add" size={24} color="white"/>
+                        </PrimaryButton>
+                    </View>
+                    <NumberContainer>{currentGuess}</NumberContainer>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                            <Ionicons name="md-remove" size={24} color="white"/>
+                        </PrimaryButton>
+                    </View>
+                </View>
+            </>
+        );
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Title>Opponent's Guess</Title>
+            {content}
             <View style={styles.listContainer}>
                 <FlatList
                     data={guessRounds}
@@ -89,7 +117,8 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 12
+        padding: 12,
+        alignItems: 'center'
     },
     actionButtonsContainer: {
         flexDirection: 'row'
@@ -103,5 +132,9 @@ const styles = StyleSheet.create({
     listContainer: {
         flex: 1,
         padding: 16
+    },
+    buttonContainerWide: {
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import {TextInput, View, StyleSheet, Alert, Text} from 'react-native';
+import {TextInput, View, StyleSheet, Alert, useWindowDimensions, KeyboardAvoidingView, ScrollView} from 'react-native';
 import PrimaryButton from '../components/UI/PrimaryButton';
 import {useState} from 'react';
 import Colors from '../constants/colors';
@@ -9,6 +9,8 @@ import InstructionText from '../components/UI/InstructionText';
 function StartGameScreen({onPickNumber}) {
 
     const [enteredNumber, setEnteredNumber] = useState('');
+
+    const height = useWindowDimensions().height;
 
     function numberInputHandler(enteredText) {
         setEnteredNumber(enteredText);
@@ -32,50 +34,61 @@ function StartGameScreen({onPickNumber}) {
         onPickNumber(chosenNumber);
     }
 
+    const marginTopDistance = height < 400 ? 30 : 100;
+
     return (
-        <View style={styles.container}>
-            <Title>Guess My Number</Title>
-            <Card>
-                <InstructionText>Enter a number</InstructionText>
-                <TextInput
-                    style={styles.input}
-                    maxLength={2}
-                    keyboardType={'number-pad'}
-                    value={enteredNumber}
-                    onChangeText={numberInputHandler}/>
-                <View style={styles.actionButtonsContainer}>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-                    </View>
+        <ScrollView style={styles.screen}>
+            <KeyboardAvoidingView style={styles.screen} behavior="position">
+                <View style={[styles.container, {marginTop: marginTopDistance}]}>
+                    <Title>Guess My Number</Title>
+                    <Card>
+                        <InstructionText>Enter a number</InstructionText>
+                        <TextInput
+                            style={styles.input}
+                            maxLength={2}
+                            keyboardType={'number-pad'}
+                            value={enteredNumber}
+                            onChangeText={numberInputHandler}/>
+                        <View style={styles.actionButtonsContainer}>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                            </View>
+                        </View>
+                    </Card>
                 </View>
-            </Card>
-        </View>
+            </KeyboardAvoidingView>
+        </ScrollView>
     );
 }
 
+// const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       marginTop: 100,
-       alignItems: 'center'
-   },
-   inputContainer: {
-       justifyContent: 'center',
-       alignItems: 'center',
-       marginTop: 36,
-       marginHorizontal: 24,
-       padding: 16,
-       backgroundColor: Colors.primary800,
-       borderRadius: 8,
-       elevation: 20,
-       shadowColor: 'black',
-       shadowOffset: {width: 0, height: 2},
-       shadowRadius: 6,
-       shadowOpacity: 0.25
-   },
+    screen: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        marginTop: 100,
+        alignItems: 'center'
+    },
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 36,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: Colors.primary800,
+        borderRadius: 8,
+        elevation: 20,
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
     input: {
         height: 50,
         width: 50,
@@ -88,10 +101,10 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     actionButtonsContainer: {
-       flexDirection: 'row'
+        flexDirection: 'row'
     },
     buttonContainer: {
-       flex: 1
+        flex: 1
     }
 });
 
